1e5475ecea5c110abfee34dad0ab742ed21f4dc6
赵星明 <zhaoxingming@olading.com>
2024-07-24 22:46:06 +0800

    chore: 格式化

--- docs/.vuepress/components/guide/customize/header-title.vue
+++ docs/.vuepress/components/guide/customize/header-title.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="center">
-    <v-calendar>
-      <template v-slot:header-title="{ shortMonthLabel, shortYearLabel }">
-        <span> {{ shortMonthLabel }} '{{ shortYearLabel }}</span>
-      </template>
-    </v-calendar>
-  </div>
+    <div class="center">
+        <v-calendar>
+            <template v-slot:header-title="{ shortMonthLabel, shortYearLabel }">
+                <span> {{ shortMonthLabel }} '{{ shortYearLabel }}</span>
+            </template>
+        </v-calendar>
+    </div>
 </template>
